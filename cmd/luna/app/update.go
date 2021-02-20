e9a91659d6eb804200ba4668eb16758cd3e91b7f
Ethan Tang <tang_yanhan@126.com>
2021-02-20 14:33:31 +0800

    Use builtin updates instead of doing it manually.

--- cmd/luna/app/update.go
+++ cmd/luna/app/update.go
@@ -2,14 +2,9 @@ package app
 
 import (
 	"context"
-	"fmt"
-	"net/http"
 
-	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/pkg/errors"
 	"github.com/quay/claircore/libvuln"
-	"github.com/quay/claircore/libvuln/driver"
-	"github.com/quay/claircore/updater"
 	_ "github.com/quay/claircore/updater/defaults" // register updaters. Only available >= v0.3.1
 	"github.com/spf13/cobra"
 )
@@ -34,31 +29,21 @@ func NewUpdateCommand() *cobra.Command {
 
 // Update do an update online
 func Update(ctx context.Context, dsn string) error {
-	pool, err := pgxpool.Connect(ctx, dsn)
-	if err != nil {
-		return err
+	opts := &libvuln.Opts{
+		ConnString:  dsn,
+		Migrations:  true,
+		MaxConnPool: 32,
 	}
-	defer pool.Close()
+
 	// do this simply to initialize tables if not exists
-	if _, err := libvuln.New(ctx, &libvuln.Opts{
-		ConnString: dsn,
-		Migrations: true,
-	}); err != nil {
+	vuln, err := libvuln.New(ctx, opts)
+	if err != nil {
 		return errors.Wrap(err, "failed to setup index")
 	}
-	u, err := libvuln.NewUpdater(pool, http.DefaultClient, nil, 4, nil)
+	err = vuln.FetchUpdates(ctx)
 	if err != nil {
 		return errors.Wrap(err, "failed to create new updater")
 	}
 
-	d := updater.Registered()
-	ufs := make([]driver.UpdaterSetFactory, 0, len(d))
-	for _, f := range d {
-		ufs = append(ufs, f)
-	}
-	fmt.Println("DBG - updater num=", len(ufs))
-	if err := u.RunUpdaters(ctx, ufs...); err != nil {
-		return errors.Wrap(err, "failed to run updaters")
-	}
 	return nil
 }
