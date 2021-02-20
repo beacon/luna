9ff4c65cb05f6a121eaf6029c33667ba364798cb
Ethan Tang <tang_yanhan@126.com>
2021-02-20 13:55:40 +0800

    Updated claircore to v0.3.1

--- cmd/luna/app/update.go
+++ cmd/luna/app/update.go
@@ -7,17 +7,10 @@ import (
 
 	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/pkg/errors"
-	"github.com/quay/claircore/alpine"
-	"github.com/quay/claircore/aws"
-	"github.com/quay/claircore/debian"
 	"github.com/quay/claircore/libvuln"
 	"github.com/quay/claircore/libvuln/driver"
-	"github.com/quay/claircore/oracle"
-	"github.com/quay/claircore/photon"
-	"github.com/quay/claircore/rhel"
-	"github.com/quay/claircore/suse"
-	"github.com/quay/claircore/ubuntu"
 	"github.com/quay/claircore/updater"
+	_ "github.com/quay/claircore/updater/defaults" // register updaters. Only available >= v0.3.1
 	"github.com/spf13/cobra"
 )
 
@@ -57,9 +50,7 @@ func Update(ctx context.Context, dsn string) error {
 	if err != nil {
 		return errors.Wrap(err, "failed to create new updater")
 	}
-	if err := registerUpdaters(ctx); err != nil {
-		return errors.Wrap(err, "failed to register updaters")
-	}
+
 	d := updater.Registered()
 	ufs := make([]driver.UpdaterSetFactory, 0, len(d))
 	for _, f := range d {
@@ -69,27 +60,5 @@ func Update(ctx context.Context, dsn string) error {
 	if err := u.RunUpdaters(ctx, ufs...); err != nil {
 		return errors.Wrap(err, "failed to run updaters")
 	}
-	return nil
-}
-
-// It's kind of weird that package defaults is not present in claircore package
-// Interesting to find out more details
-func registerUpdaters(ctx context.Context) error {
-	rf, err := rhel.NewFactory(ctx, rhel.DefaultManifest)
-	if err != nil {
-		return err
-	}
-	updater.Register("rhel", rf)
-
-	updater.Register("ubuntu", &ubuntu.Factory{Releases: ubuntu.Releases})
-	updater.Register("alpine", driver.UpdaterSetFactoryFunc(alpine.UpdaterSet))
-	updater.Register("aws", driver.UpdaterSetFactoryFunc(aws.UpdaterSet))
-	updater.Register("debian", driver.UpdaterSetFactoryFunc(debian.UpdaterSet))
-	updater.Register("oracle", driver.UpdaterSetFactoryFunc(oracle.UpdaterSet))
-	updater.Register("photon", driver.UpdaterSetFactoryFunc(photon.UpdaterSet))
-
-	// Strange...
-	// updater.Register("pyupio", driver.UpdaterSetFactoryFunc(pyupio.UpdaterSet))
-	updater.Register("suse", driver.UpdaterSetFactoryFunc(suse.UpdaterSet))
 	return nil
 }
